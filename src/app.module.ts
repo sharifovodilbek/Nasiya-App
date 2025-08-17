097af2dc3d2eb90093757a9bd55db3764700daee
sharifovodilbek <odilbek5060@gmail.com>
2025-08-17 15:14:12 +0500

    changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,9 +10,25 @@ import { SampleModule } from './sample/sample.module';
 import { SmsModule } from './sms/sms.module';
 import { MulterModule } from './multer/multer.module';
 import { PaymentsModule } from './payments/payments.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [AuthModule,PrismaModule,SellerModule,DebtorModule, DebtModule,PaymentsModule,SampleModule, SmsModule, MulterModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/api/uploads', 
+    }),
+    AuthModule,
+    PrismaModule,
+    SellerModule,
+    DebtorModule,
+    DebtModule,
+    PaymentsModule,
+    SampleModule,
+    SmsModule,
+    MulterModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
